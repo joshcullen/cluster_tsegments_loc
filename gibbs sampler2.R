628ea70977fafc424fa0eb94850baedc9c545754
Josh Cullen <joshcullen@Joshs-MacBook-Pro-3.local>
2019-09-30 15:40:56 -0400

    Evaluation of output from store.theta[] matrix

--- gibbs sampler2.R
+++ gibbs sampler2.R
@@ -3,9 +3,13 @@ set.seed(1)
 
 library('Rcpp')
 sourceCpp('aux1.cpp')
-source('gibbs functions.R')
+source('gibbs functions.R') #for clustering
 
-dat=read.csv('fake data.csv',as.is=T)
+
+
+## ID 1 ##
+
+dat=read.csv('ID1 Seg x Loc.csv',header =T, sep = ",")
 dat=data.matrix(dat)
 n=rowSums(dat)
 nobs=nrow(dat)
@@ -17,7 +21,7 @@ psi=0.01
 gamma1=0.1
 
 #starting values
-nclustmax=nloc
+nclustmax=nobs
 z=sample(1:nclustmax,size=nobs,replace=T)
 theta=matrix(1/nloc,nclustmax,nloc)
 phi=rep(1/nclustmax,nclustmax)
@@ -68,3 +72,260 @@ for (i in 1:ngibbs){
   store.phi[i,]=phi
   store.z[i,]=z
 }
+
+
+
+plot(store.loglikel,type='l')
+
+plot(store.phi[ngibbs,],type='h')
+plot(store.theta[ngibbs,],type='h')
+plot(store.z[ngibbs,],type='h')
+
+
+
+
+
+
+
+
+
+
+## ID 12 ##
+
+dat=read.csv('ID12 Seg x Loc.csv',header =T, sep = ",")
+dat=data.matrix(dat)
+n=rowSums(dat)
+nobs=nrow(dat)
+nloc=ncol(dat)
+lo=0.000000000000001
+
+#priors
+psi=0.01
+gamma1=0.1
+
+#starting values
+nclustmax=nobs
+z=sample(1:nclustmax,size=nobs,replace=T)
+theta=matrix(1/nloc,nclustmax,nloc)
+phi=rep(1/nclustmax,nclustmax)
+
+#store results
+ngibbs=1000
+store.phi=matrix(NA,ngibbs,nclustmax)
+store.z=matrix(NA,ngibbs,nobs)
+store.theta=matrix(NA,ngibbs,nclustmax*nloc)
+store.loglikel=matrix(NA,ngibbs,1)
+
+#gibbs sampler
+nburn=ngibbs/2
+for (i in 1:ngibbs){
+  print(i)
+  
+  #occasionally re-order this
+  if (i<nburn & i%%50==0){
+    ind=order(phi,decreasing=T)
+    theta=theta[ind,]
+    phi=phi[ind]
+    znew=z
+    for (j in 1:nclustmax){
+      znew[z==ind[j]]=j
+    }
+    z=znew
+  }
+  
+  #draw samples from FCD's
+  z=sample.z(dat=dat,theta=theta,phi=phi,
+             nobs=nobs,nclustmax=nclustmax,nloc=nloc,z=z,n=n)
+  # z=z.true
+  
+  v=sample.v(z=z,nclustmax=nclustmax,gamma1=gamma1)
+  phi=GetPhi(vec=c(v,1),nclustmax=nclustmax)
+  
+  theta=sample.theta(dat=dat,nclustmax=nclustmax,nloc=nloc,z=z,psi=psi)
+  #to avoid numerical issues
+  theta[theta<lo]=lo
+  # theta=theta.true
+  
+  #get logl
+  tmp=sum(dat*log(theta)[z,])+sum(dbeta(v,1,gamma1,log=T))+sum((psi-1)*log(theta))
+  
+  #store results
+  store.loglikel[i]=tmp
+  store.theta[i,]=theta
+  store.phi[i,]=phi
+  store.z[i,]=z
+}
+
+
+
+plot(store.loglikel,type='l')
+
+plot(store.phi[ngibbs,],type='h')
+plot(store.theta[ngibbs,],type='h')
+plot(store.z[ngibbs,],type='h')
+
+
+
+
+
+
+
+
+
+
+## ID 19 ##
+
+dat=read.csv('ID19 Seg x Loc.csv',header =T, sep = ",")
+dat=data.matrix(dat)
+n=rowSums(dat)
+nobs=nrow(dat)
+nloc=ncol(dat)
+lo=0.000000000000001
+
+#priors
+psi=0.01
+gamma1=0.1
+
+#starting values
+nclustmax=nobs
+z=sample(1:nclustmax,size=nobs,replace=T)
+theta=matrix(1/nloc,nclustmax,nloc)
+phi=rep(1/nclustmax,nclustmax)
+
+#store results
+ngibbs=1000
+store.phi=matrix(NA,ngibbs,nclustmax)
+store.z=matrix(NA,ngibbs,nobs)
+store.theta=matrix(NA,ngibbs,nclustmax*nloc)
+store.loglikel=matrix(NA,ngibbs,1)
+
+#gibbs sampler
+nburn=ngibbs/2
+for (i in 1:ngibbs){
+  print(i)
+  
+  #occasionally re-order this
+  if (i<nburn & i%%50==0){
+    ind=order(phi,decreasing=T)
+    theta=theta[ind,]
+    phi=phi[ind]
+    znew=z
+    for (j in 1:nclustmax){
+      znew[z==ind[j]]=j
+    }
+    z=znew
+  }
+  
+  #draw samples from FCD's
+  z=sample.z(dat=dat,theta=theta,phi=phi,
+             nobs=nobs,nclustmax=nclustmax,nloc=nloc,z=z,n=n)
+  # z=z.true
+  
+  v=sample.v(z=z,nclustmax=nclustmax,gamma1=gamma1)
+  phi=GetPhi(vec=c(v,1),nclustmax=nclustmax)
+  
+  theta=sample.theta(dat=dat,nclustmax=nclustmax,nloc=nloc,z=z,psi=psi)
+  #to avoid numerical issues
+  theta[theta<lo]=lo
+  # theta=theta.true
+  
+  #get logl
+  tmp=sum(dat*log(theta)[z,])+sum(dbeta(v,1,gamma1,log=T))+sum((psi-1)*log(theta))
+  
+  #store results
+  store.loglikel[i]=tmp
+  store.theta[i,]=theta
+  store.phi[i,]=phi
+  store.z[i,]=z
+}
+
+
+
+plot(store.loglikel,type='l')
+
+plot(store.phi[ngibbs,],type='h')
+plot(store.theta[ngibbs,],type='h')
+plot(store.z[ngibbs,],type='h')
+
+
+
+
+
+
+
+
+
+
+## ID 27 ##
+
+dat=read.csv('ID27 Seg x Loc.csv',header =T, sep = ",")
+dat=data.matrix(dat)
+n=rowSums(dat)
+nobs=nrow(dat)
+nloc=ncol(dat)
+lo=0.000000000000001
+
+#priors
+psi=0.01
+gamma1=0.1
+
+#starting values
+nclustmax=nobs
+z=sample(1:nclustmax,size=nobs,replace=T)
+theta=matrix(1/nloc,nclustmax,nloc)
+phi=rep(1/nclustmax,nclustmax)
+
+#store results
+ngibbs=1000
+store.phi=matrix(NA,ngibbs,nclustmax)
+store.z=matrix(NA,ngibbs,nobs)
+store.theta=matrix(NA,ngibbs,nclustmax*nloc)
+store.loglikel=matrix(NA,ngibbs,1)
+
+#gibbs sampler
+nburn=ngibbs/2
+for (i in 1:ngibbs){
+  print(i)
+  
+  #occasionally re-order this
+  if (i<nburn & i%%50==0){
+    ind=order(phi,decreasing=T)
+    theta=theta[ind,]
+    phi=phi[ind]
+    znew=z
+    for (j in 1:nclustmax){
+      znew[z==ind[j]]=j
+    }
+    z=znew
+  }
+  
+  #draw samples from FCD's
+  z=sample.z(dat=dat,theta=theta,phi=phi,
+             nobs=nobs,nclustmax=nclustmax,nloc=nloc,z=z,n=n)
+  # z=z.true
+  
+  v=sample.v(z=z,nclustmax=nclustmax,gamma1=gamma1)
+  phi=GetPhi(vec=c(v,1),nclustmax=nclustmax)
+  
+  theta=sample.theta(dat=dat,nclustmax=nclustmax,nloc=nloc,z=z,psi=psi)
+  #to avoid numerical issues
+  theta[theta<lo]=lo
+  # theta=theta.true
+  
+  #get logl
+  tmp=sum(dat*log(theta)[z,])+sum(dbeta(v,1,gamma1,log=T))+sum((psi-1)*log(theta))
+  
+  #store results
+  store.loglikel[i]=tmp
+  store.theta[i,]=theta
+  store.phi[i,]=phi
+  store.z[i,]=z
+}
+
+
+
+plot(store.loglikel,type='l')
+
+plot(store.phi[ngibbs,],type='h')
+plot(store.theta[ngibbs,],type='h')
+plot(store.z[ngibbs,],type='h')
